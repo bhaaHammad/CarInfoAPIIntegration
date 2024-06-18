453a72c28f3f47918ed35ce70e5b060eb6643328
bhaaHammad <bhaahammad0@gmail.com>
2024-06-19 01:57:37 +0300

    Add project files.

---
+++ CarInfoRetrievalService/Controllers/CarLookupController.cs
@@ -0,0 +1,25 @@
+﻿using CarInfoRetrievalService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarInfoRetrievalService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarLookupController : ControllerBase
+    {
+        private readonly CarMakeIdentifierService _carMakeIdentifierService;
+
+        public CarLookupController(CarMakeIdentifierService carMakeServiCarMakeIdentifierService)
+        {
+            _carMakeIdentifierService = carMakeServiCarMakeIdentifierService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCarModelsByMakeAndYear([FromQuery] string make,
+                                                                   [FromQuery] int modelYear)
+        {
+            var makeId = _carMakeIdentifierService.FindMakeIdByMakeName(make);
+            return Ok(makeId);
+        }
+    }
+}
---
+++ CarInfoRetrievalService/Services/CarMakeIdentifierService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+
+namespace CarInfoRetrievalService.Services
+{
+    public class CarMakeIdentifierService
+    {
+        private const string FilePath = "CarMake.csv";
+        public string? FindMakeIdByMakeName(string makeName)
+        {
+            using (var streamReader = new StreamReader(FilePath))
+            {
+                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                {
+                    while (csvReader.Read())
+                    {
+                        var carMake = csvReader.GetRecord<dynamic>();
+                        var carMakeName = carMake != null ? carMake.make_name : null;
+                        if (carMakeName != null && carMakeName == makeName.ToUpper())
+                        {
+                            return carMake != null ? carMake.make_id : null;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
